0bbfa9c958f9bd4d2100482518ba524f29941458
TQ的Dell笔记本 <361957647@qq.com>
2020-03-02 23:03:54 +0800

    完成了一个Django框架创建的Web应用程序，用户名timi，密码12345678

---
+++ python_crash_course/learning_log/learning_logs/urls.py
@@ -0,0 +1,14 @@
+"""定义learning_logs的URL模式"""
+
+from django.conf.urls import url
+from . import  views
+urlpatterns=[
+    #主页
+    # 正则表达式，让python查找从开头到末尾没任何东西的url
+    # 当与正则表达式匹配时，调用这个视图
+    # 指定这个url模式名字为index
+    url(r'^$',views.index,name='index'),
+    url(r'^topics/$',views.topics,name='topics'),
+    # 匹配两个斜杠中的整数，并把该证书存储在topic_id中，d+表示任意位数的整数
+    url(r'^topics/(?P<topic_id>\d+)/$',views.topic,name='topic')
+]
--- python_crash_course/learning_log/learning_logs/views.py
+++ python_crash_course/learning_log/learning_logs/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+from learning_logs.models import Topic
+
+
+def index(request):
+    """学习笔记的主页"""
+    return render(request,'learning_logs/index.html') #原始请求对象，一个可用于创建网页的模板
+
+def topics(request):
+    """用于显示所有topic的目录页"""
+    # 查询数据库并排序，将查询集存储在变量
+    topics=Topic.objects.order_by('date_added')
+    # 要发送给模板的上下文，一个字典，值是模板要访问的数据
+    context={'topics':topics}
+    return render(request,'learning_logs/topics.html',context)
+
+def topic(request,topic_id):
+    # 显示单个主题的所有条目
+    topic =Topic.objects.get(id=topic_id)
+    entries=topic.entry_set.order_by('-date_added') #倒序
+    context={'topic':topic,'entries':entries}
+    return render(request,'learning_logs/topic.html',context)
