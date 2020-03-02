e37871802b4116ef7130305a1355f910c5278812
TQ的Dell笔记本 <361957647@qq.com>
2020-03-02 20:15:42 +0800

    创建了一个learning_log的Django项目

---
+++ python_crash_course/learning_log/learning_logs/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+
+class Topic(models.Model):
+    """主题的类"""
+    text=models.CharField(max_length=200)
+    date_added=models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        """返回模型的字符串表示"""
+        return self.text
+
+class Entry(models.Model):
+    """条目的类"""
+    # 外键，引用数据库的另一条记录
+    topic = models.ForeignKey(Topic,on_delete=models.CASCADE)
+    # 不限制长度的文本
+    text=models.TextField()
+    date_added=models.DateTimeField(auto_now_add=True)
+    # 嵌套的meta类，verbose_name_plural表示复数的词汇
+    class Meta:
+        verbose_name_plural='entries'
+    def __str__(self):
+        # 只显示前50个字符
+        return self.text[:50]+"..."
