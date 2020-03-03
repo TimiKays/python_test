8eba0b46a5730df3d2fdbd5820c8fee9e6d2ec48
TQ的Dell笔记本 <361957647@qq.com>
2020-03-03 12:02:55 +0800

    用户可以添加新主题了，接下来写用户可以添加新条目

--- python_crash_course/learning_log/learning_logs/forms.py
+++ python_crash_course/learning_log/learning_logs/forms.py
@@ -5,7 +5,7 @@ class TopicForm(forms.ModelForm):
     class Meta:
         """告诉Django根据哪个模型创建表单，在表单中包含哪些字段"""
         model=Topic
-        # 表单的字段text
+        # 表单的字段只有一个，text
         fields=['text']
         # 不要为字段生成标签
         labels={'text':''}
--- python_crash_course/learning_log/learning_logs/views.py
+++ python_crash_course/learning_log/learning_logs/views.py
@@ -1,27 +1,47 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+from .models import Topic
+from .forms import TopicForm
+
 
 # Create your views here.
-from learning_logs.models import Topic
-
-
 def index(request):
     """学习笔记的主页"""
-    return render(request,'learning_logs/index.html') #原始请求对象，一个可用于创建网页的模板
+    return render(request, 'learning_logs/index.html')  # 原始请求对象，一个可用于创建网页的模板
+
 
 def topics(request):
     """用于显示所有topic的目录页"""
     # 查询数据库并排序，将查询集存储在变量
-    topics=Topic.objects.order_by('date_added')
+    topics = Topic.objects.order_by('date_added')
     # 要发送给模板的上下文，一个字典，值是模板要访问的数据
-    context={'topics':topics}
-    return render(request,'learning_logs/topics.html',context)
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
 
-def topic(request,topic_id):
-    # 显示单个主题的所有条目
-    topic =Topic.objects.get(id=topic_id)
-    entries=topic.entry_set.order_by('-date_added') #倒序
-    context={'topic':topic,'entries':entries}
-    return render(request,'learning_logs/topic.html',context)
+
+def topic(request, topic_id):
+    """显示单个主题的所有条目"""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')  # 倒序
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
+
 
 def new_topic(request):
     """用户新增一个topic的页面"""
+    if request.method != 'POST':
+        # 是GET请求，表示未提交数据，需要创建一个空表单
+        form = TopicForm()
+    else:
+        # 是post请求，说明要提交填好的表单
+        form = TopicForm(request.POST)
+        # 如果表单都填好了，并且数据与要求的字段类型和长度符合
+        if form.is_valid():
+            # 就保存到数据库
+            form.save()
+            # 重定向到网页topics,reverse根据url模型反推url
+            return HttpResponseRedirect(reverse('learning_logs:topics'))
+    # 通过上下文把表单发给模板，显示出来
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
